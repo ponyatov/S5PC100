42442214ca662b450f78b1414015cc42835e6d85
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-21 05:37:52 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/kernel.mk
+++ mk/kernel.mk
@@ -1,13 +1,12 @@
 # kernel
 .PHONY: kernel
 KERNEL_MAKE = make ARCH=$(KERNEL_ARCH)
-KERNEL_CONFIGS = $(wildcard all/*.kernel) $(wildcard arch/*.kernel)
+KERNEL_CONFIGS  = $(wildcard all/*.kernel) $(wildcard hw/*.kernel)
+KERNEL_CONFIGS += $(wildcard arch/*.kernel) $(wildcard cpu/*.kernel)
 kernel: $(TMP)/kernel/.config
 	cd $(TMP)/$@ ; $(KERNEL_MAKE) menuconfig
 $(TMP)/kernel/.config: mk/kernel.mk $(KERNEL_CONFIGS) $(REF)/$(LINUX)/README.md
-	$(MAKE) $(TMP)/kernel
 	cd $(REF)/$(LINUX) ; $(KERNEL_MAKE) O=$(TMP)/kernel allnoconfig
 	cat $(KERNEL_CONFIGS) >> $@
-	echo "CONFIG_LOCALVERSION=\"-$(MODULE)\"" >> $@
-$(TMP)/kernel:
-	rm -rf $(TMP)/kernel ; mkdir $(TMP)/kernel
+	echo     "CONFIG_LOCALVERSION=\"-$(HW)\"" >> $@
+	echo "CONFIG_DEFAULT_HOSTNAME=\"$(HW)\""  >> $@
