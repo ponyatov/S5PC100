7bc9bd6adeac3501430bd17b55ed1f85c11900ca
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-21 06:02:56 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/kernel.mk
+++ mk/kernel.mk
@@ -5,6 +5,7 @@ KERNEL_CONFIGS  = $(wildcard all/*.kernel) $(wildcard hw/*.kernel)
 KERNEL_CONFIGS += $(wildcard arch/*.kernel) $(wildcard cpu/*.kernel)
 kernel: $(TMP)/kernel/.config
 	cd $(TMP)/$@ ; $(KERNEL_MAKE) menuconfig
+	cd $(TMP)/$@ ; $(KERNEL_MAKE)
 $(TMP)/kernel/.config: mk/kernel.mk $(KERNEL_CONFIGS) $(REF)/$(LINUX)/README.md
 	cd $(REF)/$(LINUX) ; $(KERNEL_MAKE) O=$(TMP)/kernel allnoconfig
 	cat $(KERNEL_CONFIGS) >> $@
