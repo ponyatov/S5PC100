10faf87dc27d6bb8338aa8ff8483137d6c54bac3
Dmitry Ponyatov <dponyatov@gmail.com>
2024-11-21 04:52:35 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- arch/CortexA8.mk
+++ arch/CortexA8.mk
@@ -1,7 +1,6 @@
-ARCH = arm-linux-gnueabihf
+TARGET      = arm-linux-gnueabihf
+KERNEL_ARCH = arm
 
 QEMU = qemu-system-arm
 # QEMU_CFG += -M cubieboard -cpu cortex-a8
 QEMU_CFG += -M realview-pb-a8 -cpu cortex-a8
-
-KERNEL_ARCH = arm
--- mk/kernel.mk
+++ mk/kernel.mk
@@ -1,8 +1,13 @@
 # kernel
 .PHONY: kernel
-kernel: $(TMP)/.config
-	cd $(TMP)/$@ ; make menuconfig
-$(TMP)/.config: $(REF)/$(LINUX)/README.md
-	rm -rf $(TMP)/$@ ; mkdir $(TMP)/$@ ;\
-	cd $(REF)/$(LINUX) ; make O=$(TMP)/$@ ARCH=$(KERNEL_ARCH) allnoconfig
-	touch $@
+KERNEL_MAKE = make ARCH=$(KERNEL_ARCH)
+KERNEL_CONFIGS = $(wildcard all/*.kernel) $(wildcard arch/*.kernel)
+kernel: $(TMP)/kernel/.config
+	cd $(TMP)/$@ ; $(KERNEL_MAKE) menuconfig
+$(TMP)/kernel/.config: mk/kernel.mk $(KERNEL_CONFIGS) $(REF)/$(LINUX)/README.md
+	$(MAKE) $(TMP)/kernel
+	cd $(REF)/$(LINUX) ; $(KERNEL_MAKE) O=$(TMP)/kernel allnoconfig
+	cat $(KERNEL_CONFIGS) >> $@
+	echo "CONFIG_LOCALVERSION=\"-$(MODULE)\"" >> $@
+$(TMP)/kernel:
+	rm -rf $(TMP)/kernel ; mkdir $(TMP)/kernel
